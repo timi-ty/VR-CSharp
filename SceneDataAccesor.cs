cd90739eafcc3714835931ae6fcd607fe1ab1045
Timi Tayo <timilehin.ty@gmail.com>
2020-02-08 14:58:15 +0100

    Filled In

--- SceneDataAccesor.cs
+++ SceneDataAccesor.cs
@@ -12,11 +12,20 @@ public class SceneDataAccesor
     {
         DataObject[] antiqueObjects = new DataObject[1];
         antiqueObjects[0] = new DataObject();
-        antiqueObjects[0].question = "";
-        antiqueObjects[0].option1 = "";
-        antiqueObjects[0].option2 = "";
+        antiqueObjects[0].question = "Hey, friend I heard about your parent’s anniversary";
+        antiqueObjects[0].option1 = "Yes, I’m looking forward to it.";
+        antiqueObjects[0].option2 = "Yes, I need some help planning.";
         antiqueObjects[0].failureExplanation = "";
         antiqueObjects[0].successExplanation = "!!!!";
+
+        antiqueObjects[2] = new DataObject();
+        antiqueObjects[2].question = "Do you want a link to a Facebook group that sells what you are looking for at very affordable rates?";
+        antiqueObjects[2].option1 = "Sure, I’m in.";
+        antiqueObjects[2].option2 = "Nah, I’m not into that sort of thing.";
+        antiqueObjects[2].failureExplanation = "You should avoid the use of social media to purchase controlled goods.";
+        antiqueObjects[2].successExplanation = "Good choice, you should avoid the use of social media to purchase controlled goods";
+
+
 
         dict.Add("C-Antique", antiqueObjects);
     }
